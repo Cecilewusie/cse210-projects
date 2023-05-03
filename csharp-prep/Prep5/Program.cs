f9a3b8fcbc3be387a732796bb7f8d67fb5166abf
Cecilewusie <Hendrixcecil0@gmail.com>
2023-05-03 14:52:21 +0000

    Finish prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -3,7 +3,68 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+    {   
+        //calling the displayWelcome function 
+        DisplayWelcome();
+
+        //calling the PromptUsersname function
+        string userName = PromptUserName();
+
+        //Calling the PromptUserNumber function
+        int userNumber = PromptUserNumber();
+
+        // Calling the SquareNumber function by parsing an argument
+        // Console.Write("Enter a number to calculate square number for: ");
+        // string userInp = Console.ReadLine();
+        // int convertedSquare = int.Parse(userInp);
+
+        int squareNum = SquareNumber(userNumber);
+
+        //calling the DisplayResult function
+        DisplayResult(userName, squareNum);
+
+
+
+
+        //Display the message, "Welcome to the program"
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program");
+        }
+
+
+        //Asks for and returns the user's name(as a string)
+        static string PromptUserName()
+        {
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+        
+        //Asks for and returns the user's favorite number(as an interger)
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("Please enter your favorite number: ");
+            string userInput = Console.ReadLine();
+            int favoritNum = int.Parse(userInput);
+
+            return favoritNum;
+        }
+
+        //Accepts an integer as a parameter and returns that number squared(as an integer)
+        static int SquareNumber(int userNumber)
+        {
+            int square = userNumber * userNumber;
+
+            return square;
+
+        }
+
+        //Accepts the user's name and the squared number and displays them
+        static void DisplayResult(string userName, int squareNum)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {squareNum}");
+        }
     }
 }
